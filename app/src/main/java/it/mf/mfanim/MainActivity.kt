df81dad333e893c45053215030fd8e855ea48d1b
StefanoBelli <stefano9913@gmail.com>
2021-05-23 11:51:43 +0200

    introslider

--- app/src/main/java/it/mf/mfanim/MainActivity.kt
+++ app/src/main/java/it/mf/mfanim/MainActivity.kt
@@ -1,11 +1,89 @@
 package it.mf.mfanim
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.view.View
+import android.widget.TextView
+import androidx.viewpager2.widget.ViewPager2
+import it.mf.mfanim.adapter.SliderAdapter
+import it.mf.mfanim.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val SHPREFS = "it.mf.mfanim_rand409371243124_shpref"
+        private const val INTROED_KEY = "introed"
+        private val INTRO_LAYOUTS = intArrayOf(
+            R.layout.intro_first_hello,
+            R.layout.intro_second_anim_shown,
+            R.layout.intro_third_whoweare)
+    }
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        if(getSharedPreferences(SHPREFS, Context.MODE_PRIVATE)
+                .getBoolean(INTROED_KEY, false)) {
+            //start new activity
+        } else {
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+            init()
+        }
+    }
+
+    private fun init() {
+        binding.pager.adapter = SliderAdapter(INTRO_LAYOUTS)
+        binding.pager.registerOnPageChangeCallback(pageChangeCallback)
+        binding.btnNext.setOnClickListener { introCurrent(true) }
+        binding.btnPrev.setOnClickListener { introCurrent(false) }
+        makeDots(0)
+    }
+
+    private var pageChangeCallback:
+            ViewPager2.OnPageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+            makeDots(position)
+            if (position == INTRO_LAYOUTS.size - 1) {
+                binding.btnNext.text = getString(R.string.view_anims)
+            } else {
+                binding.btnNext.text = getString(R.string.next)
+                binding.btnPrev.visibility = if(position == 0) View.INVISIBLE else View.VISIBLE
+            }
+        }
+    }
+
+    private fun introCurrent(goNext : Boolean) {
+        val current = binding.pager.currentItem + if (goNext) 1 else -1
+        if (current < INTRO_LAYOUTS.size) {
+            binding.pager.currentItem = current
+        } else {
+            //start new activity
+            getSharedPreferences(SHPREFS, Context.MODE_PRIVATE).edit().apply {
+                putBoolean(INTROED_KEY, true)
+                apply()
+            }
+        }
+    }
+
+    private fun makeDots(cur : Int) {
+        val dots : Array<TextView?> = arrayOfNulls(INTRO_LAYOUTS.size)
+
+        val colorsActive = resources.getIntArray(R.array.array_dot_active)
+        val colorsInactive = resources.getIntArray(R.array.array_dot_inactive)
+
+        binding.layoutDots.removeAllViews()
+        for (i in dots.indices) {
+            dots[i] = TextView(this)
+            dots[i]!!.text = Html.fromHtml("&#8226;", 0)
+            dots[i]!!.textSize = 35f
+            dots[i]!!.setTextColor(colorsInactive[cur])
+            binding.layoutDots.addView(dots[i])
+        }
+        if (dots.isNotEmpty()) dots[cur]!!.setTextColor(colorsActive[cur])
     }
 }
---
+++ app/src/main/java/it/mf/mfanim/adapter/ViewsSliderAdapter.kt
@@ -0,0 +1,29 @@
+package it.mf.mfanim.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class SliderAdapter(private val layouts: IntArray) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(viewType, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return layouts[position]
+    }
+
+    override fun getItemCount(): Int {
+        return layouts.size
+    }
+}
