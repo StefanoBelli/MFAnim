821778e437b61f8a1e5a4f70f22c9a6dffad79a5
StefanoBelli <sa.belli@hotmail.it>
2021-05-30 22:36:31 +0200

    changes

--- app/src/main/java/it/mobileflow/mfanim/transformer/SliderTransformer.kt
+++ app/src/main/java/it/mobileflow/mfanim/transformer/SliderTransformer.kt
@@ -2,10 +2,11 @@ package it.mobileflow.mfanim.transformer
 
 import android.view.View
 import androidx.viewpager2.widget.ViewPager2
+import kotlin.math.abs
 
 class SliderTransformer : ViewPager2.PageTransformer {
     override fun transformPage(page: View, position: Float) {
-        val scale = if (position < 0.0f) position + 1.0f else Math.abs(1.0f - position)
+        val scale = if (position < 0.0f) position + 1.0f else abs(1.0f - position)
         page.scaleX = scale
         page.scaleY = scale
         page.pivotX = page.width.toFloat() * 0.5f
