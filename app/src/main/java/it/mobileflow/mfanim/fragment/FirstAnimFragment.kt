3c4cf2ef4b0b7966f37de65556fc1e10aade8279
StefanoBelli <stefano9913@gmail.com>
2021-05-31 07:38:13 +0200

    first anim

--- app/src/main/java/it/mobileflow/mfanim/fragment/FirstAnimFragment.kt
+++ app/src/main/java/it/mobileflow/mfanim/fragment/FirstAnimFragment.kt
@@ -3,6 +3,7 @@ package it.mobileflow.mfanim.fragment
 import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.graphics.Path
+import android.graphics.RectF
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +14,11 @@ import it.mobileflow.mfanim.R
 import it.mobileflow.mfanim.databinding.FragmentFirstAnimBinding
 
 class FirstAnimFragment : Fragment() {
+    companion object {
+        private const val TRSL_X = "translationX"
+        private const val TRSL_Y = "translationY"
+    }
+
     private lateinit var binding : FragmentFirstAnimBinding
     private var animDuration : Long = 0
 
@@ -50,15 +56,37 @@ class FirstAnimFragment : Fragment() {
 
     inner class ButtonClickListener : View.OnClickListener {
         private var currentStep = 0
+        private var origX : Float = 0f
+        private var origY : Float = 0f
 
         override fun onClick(v: View?) {
             when(currentStep) {
                 0 -> {
-                    ObjectAnimator.ofFloat(v, "translationX", 300f).apply {
+                    origX = v!!.x
+                    origY = v.y
+                    ObjectAnimator.ofFloat(v, TRSL_X, 300f).apply {
                         duration = animDuration
                         start()
                     }
                     currentStep = 1
+                }
+                1 -> {
+                    ObjectAnimator.ofFloat(v, TRSL_Y, 300f).apply {
+                        duration = animDuration
+                        start()
+                    }
+                    currentStep = 2
+                }
+                2 -> {
+                    val linedPath = Path().apply {
+                        moveTo(v!!.x, v.y)
+                        lineTo(origX, origY)
+                    }
+                    ObjectAnimator.ofFloat(v, View.X, View.Y, linedPath).apply {
+                        duration = animDuration
+                        start()
+                    }
+                    currentStep = 0
                 }
             }
         }
