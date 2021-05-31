4f3bcf735e99d8059360a7b5069c45ebc0e4e9f8
StefanoBelli <stefano9913@gmail.com>
2021-05-31 09:03:46 +0200

    changes

--- app/src/main/java/it/mobileflow/mfanim/AnimActivity.kt
+++ app/src/main/java/it/mobileflow/mfanim/AnimActivity.kt
@@ -1,11 +1,13 @@
 package it.mobileflow.mfanim
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import it.mobileflow.mfanim.adapter.FragmentAdapter
 import it.mobileflow.mfanim.databinding.ActivityAnimBinding
+
 
 class AnimActivity : AppCompatActivity() {
     private lateinit var binding : ActivityAnimBinding
@@ -16,20 +18,21 @@ class AnimActivity : AppCompatActivity() {
         binding = ActivityAnimBinding.inflate(layoutInflater)
         setContentView(binding.root)
         titles = arrayOf(
-            getString(R.string.first_anim_name),
-            getString(R.string.second_anim_name),
-            getString(R.string.third_anim_name),
-            getString(R.string.fourth_anim_name)
+                getString(R.string.first_anim_name),
+                getString(R.string.second_anim_name),
+                getString(R.string.third_anim_name),
+                getString(R.string.fourth_anim_name)
         )
         init()
     }
 
     private fun init() {
+        val colorDrawable = ColorDrawable(getColor(R.color.primary))
+        supportActionBar!!.setBackgroundDrawable(colorDrawable)
+        window.statusBarColor = getColor(R.color.primary_variant)
         binding.pager.adapter = FragmentAdapter(this)
-        TabLayoutMediator(binding.layoutTabs, binding.pager) {
-                tab: TabLayout.Tab,
-                position: Int ->
-                    tab.text = titles[position]
-        }.attach()
+        TabLayoutMediator(binding.layoutTabs, binding.pager)
+            { tab: TabLayout.Tab, position: Int
+                -> tab.text = titles[position] }.attach()
     }
 }
